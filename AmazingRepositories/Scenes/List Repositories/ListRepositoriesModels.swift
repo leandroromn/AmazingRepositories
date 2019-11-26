611ff8d2ae19b57f2899750a15ff0e09c44f0a1c
Leandro Romano <leandroromano.dev@gmail.com>
2019-11-25 21:53:01 -0300

    Implementation for Repository model.

--- AmazingRepositories/Scenes/List Repositories/ListRepositoriesModels.swift
+++ AmazingRepositories/Scenes/List Repositories/ListRepositoriesModels.swift
@@ -6,8 +6,44 @@
 //  Copyright (c) 2019 Leandro Romano. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
 enum ListRepositories {
 
+    struct Response: Codable {
+        
+        let repositories: [Repository]?
+        
+        enum CodingKeys: String, CodingKey {
+            case repositories = "items"
+        }
+        
+    }
+    
+}
+
+struct Repository: Codable {
+    
+    let name: String
+    let stars: Int
+    let owner: Owner
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case stars = "stargazers_count"
+        case owner
+    }
+    
+}
+
+struct Owner: Codable {
+    
+    let name: String
+    let photo: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "login"
+        case photo = "avatar_url"
+    }
+    
 }
