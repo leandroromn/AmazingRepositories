39faff8ab9254c80756a830dd36a5c534d35a505
Leandro Romano <leandroromano.dev@gmail.com>
2019-11-25 21:53:15 -0300

    Implementation for List Repositories worker.

--- AmazingRepositories/Scenes/List Repositories/ListRepositoriesWorker.swift
+++ AmazingRepositories/Scenes/List Repositories/ListRepositoriesWorker.swift
@@ -6,8 +6,15 @@
 //  Copyright (c) 2019 Leandro Romano. All rights reserved.
 //
 
-import UIKit
+import Foundation
+import PromiseKit
 
 class ListRepositoriesWorker {
 
+    let networkProvider = NetworkProvider.shared
+    
+    func getStarredRepositories() -> Promise<ListRepositories.Response> {
+        return networkProvider.request(.search(matching: "language:swift", sortedBy: .numberOfForks))
+    }
+    
 }
