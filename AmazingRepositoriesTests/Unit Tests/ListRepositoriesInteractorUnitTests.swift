1b3ed4299e942dc0229976b86978a5b8820213e9
Leandro Romano <leandroromano.dev@gmail.com>
2019-12-03 19:52:58 -0300

    Implementation for failure unit tests.

--- AmazingRepositoriesTests/Unit Tests/ListRepositoriesInteractorUnitTests.swift
+++ AmazingRepositoriesTests/Unit Tests/ListRepositoriesInteractorUnitTests.swift
@@ -38,7 +38,7 @@ class ListRepositoriesInteractorUnitTests: QuickSpec {
             
             describe("a request to fetch repositories and populate a list") {
                 
-                describe("a successful request to GitHub's API") {
+                context("a successful request to GitHub's API") {
                     
                     it("returns the first page with 30 results for most starred repositories") {
                         mockWorker.returnType = .success(type: .numberOfStars, page: 1)
@@ -106,6 +106,56 @@ class ListRepositoriesInteractorUnitTests: QuickSpec {
                         expect(sut.numberOfRows).to(equal(30))
                         expect(sut.currentPage).to(equal(1))
                         
+                    }
+                    
+                }
+                
+                context("a failure request to GitHub's API") {
+                    
+                    it("returns an error when the first request has empty data response") {
+                        mockWorker.returnType = .failure
+                        sut.filterRepositories(sortedBy: .numberOfStars)
+                        
+                        expect(sut.numberOfRows).to(equal(0))
+                        expect(sut.currentPage).to(equal(1))
+                        expect(mockPresenter.hasCalledRemoveLoadingState).to(beTrue())
+                        expect(mockPresenter.hasCalledPresentError).to(beTrue())
+                    }
+                    
+                    it("returns an error when the request for infity scroll failed") {
+                        mockWorker.returnType = .success(type: .numberOfStars, page: 1)
+                        sut.requestRepositories(sortedBy: .numberOfStars)
+                        
+                        expect(sut.numberOfRows).to(equal(30))
+                        expect(sut.currentPage).to(equal(1))
+                        expect(mockPresenter.hasCalledRemoveLoadingState).to(beTrue())
+                        expect(mockPresenter.hasCalledReloadTableView).to(beTrue())
+                        
+                        mockWorker.returnType = .parseError
+                        sut.requestNextPage(index: 20)
+                        
+                        expect(sut.numberOfRows).to(equal(30))
+                        expect(sut.currentPage).to(equal(2))
+                        expect(mockPresenter.hasCalledRemoveLoadingState).to(beTrue())
+                        expect(mockPresenter.hasCalledPresentError).to(beTrue())
+                    }
+                    
+                    it("returns an error when the request for pull to refresh failed") {
+                        mockWorker.returnType = .success(type: .numberOfStars, page: 1)
+                        sut.requestRepositories(sortedBy: .numberOfStars)
+                        
+                        expect(sut.numberOfRows).to(equal(30))
+                        expect(sut.currentPage).to(equal(1))
+                        expect(mockPresenter.hasCalledRemoveLoadingState).to(beTrue())
+                        expect(mockPresenter.hasCalledReloadTableView).to(beTrue())
+                        
+                        mockWorker.returnType = .parseError
+                        sut.refreshRepositories()
+                        
+                        expect(sut.numberOfRows).to(equal(30))
+                        expect(sut.currentPage).to(equal(1))
+                        expect(mockPresenter.hasCalledRemoveLoadingState).to(beTrue())
+                        expect(mockPresenter.hasCalledPresentError).to(beTrue())
                     }
                     
                 }
